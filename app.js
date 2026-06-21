876e02e12dbcc24394512eba893957dd78ac7c5e
prashanth7890 <prashanthmuthu777@gmail.com>
2026-06-21 06:41:41 +0000

    dev branch update

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
-console.log("Feature Branch Work");
+console.log("Dev Branch Work");
 console.log("Hello prashanth");
 git add app.js
 git commit -m "testing branch update"
+console.log("Dev Branch Work");
