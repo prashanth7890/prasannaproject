7b93b7aed661a0af1b2978eec5507fd3491cd18a
prashanth7890 <prashanthmuthu777@gmail.com>
2026-06-21 07:02:45 +0000

    fixed app.js

--- app.js
+++ app.js
@@ -1,6 +1,4 @@
 console.log("Dev Branch Work");
 console.log("Hello prashanth");
-git add app.js
-git commit -m "testing branch update"
 console.log("Dev Branch Work");
 console.log("Hello prashanth");
